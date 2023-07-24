fe92f79b47a85d52a0cbc5f5e3027e5925bc81ea
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-07-24 13:07:51 +0530

    Added a logout button and its functionality

--- app/src/main/java/com/vision/firebase/Home.kt
+++ app/src/main/java/com/vision/firebase/Home.kt
@@ -1,11 +1,29 @@
 package com.vision.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.vision.firebase.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth=FirebaseAuth.getInstance()
+        binding.logout.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        Toast.makeText(this,"Log-Out Successful",Toast.LENGTH_LONG).show()
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
